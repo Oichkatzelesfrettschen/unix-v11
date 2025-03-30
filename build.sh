9b083a5059ca3da937fb4d2b8c9a3745e490687c
H4n-uL <jun061119@gmail.com>
2025-03-30 15:47:36 +0900

    UPDATE :: Using .img instead of .cdr

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@ set -e
 
 cargo build -r --target x86_64-unknown-uefi
 mkdir -p dist/efi/boot
-cp target/x86_64-unknown-uefi/release/unixv11.efi dist/efi/boot/bootx64.efi 
-hdiutil create -fs fat32 -ov -size 64m -volname UNIXV11 -format UDTO -srcfolder dist uefi.cdr
-qemu-system-x86_64 -cpu qemu64 -bios ovmf_amd64.fd -drive file=uefi.cdr,if=ide -m 512M
+cp target/x86_64-unknown-uefi/release/unixv11.efi dist/efi/boot/bootx64.efi
+
+dd if=/dev/zero of=uefi.img bs=1m count=64
+diskno=$(hdiutil attach -imagekey diskimage-class=CRawDiskImage -nomount uefi.img | head -n 1 | awk '{print $1}')
+diskutil eraseDisk FAT32 UNIXV11 MBRFormat $diskno
+cp -R dist/* /Volumes/UNIXV11/
+hdiutil detach $diskno
+
+qemu-system-x86_64 -cpu qemu64 -bios ovmf_amd64.fd -drive file=uefi.img,if=ide -m 512M
