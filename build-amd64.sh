401fcf7d3b7e2a444920e705607aff633d046078
H4n-uL <jun061119@gmail.com>
2025-04-28 21:09:25 +0900

    UPDATE :: Updated qemu parameters

--- build-amd64.sh
+++ build-amd64.sh
@@ -2,18 +2,20 @@
 
 set -e
 
-cd efi;    cargo build -r; cd ..
-cd kernel; cargo build -r; cd ..
+cd efi;    cargo build -r --target x86_64-unknown-uefi; cd ..
+cd kernel; cargo build -r --target x86_64-unknown-none; cd ..
 
 mkdir -p dist/bin
 mkdir -p dist/efi/boot
 cp target/x86_64-unknown-uefi/release/unix-v11-efi.efi dist/efi/boot/bootx64.efi
 cp target/x86_64-unknown-none/release/unix-v11-kernel dist/bin/unix-v11
 
-dd if=/dev/zero of=unixv11.disk bs=1m count=64
+dd if=/dev/zero of=unixv11.disk bs=1M count=64
 diskno=$(hdiutil attach -imagekey diskimage-class=CRawDiskImage -nomount unixv11.disk | head -n 1 | awk '{print $1}')
 diskutil eraseDisk FAT32 UNIXV11 GPTFormat $diskno
 cp -R dist/* /Volumes/UNIXV11/
 hdiutil detach $diskno
 
-qemu-system-x86_64 -cpu qemu64 -bios ovmf_amd64.fd -drive file=unixv11.disk,if=ide -m 512M -serial stdio
+qemu-system-x86_64 -cpu Skylake-Client -machine q35 -smp 1 -bios OVMF-AMD64.fd \
+ -drive file=unixv11.disk,if=none,id=drv0,format=raw -device nvme,drive=drv0,serial=unixv11nvme \
+ -m 512M -serial stdio
