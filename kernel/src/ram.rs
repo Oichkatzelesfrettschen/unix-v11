420cc0df5c0c376fc3e911dda52ead0cdf149ce4
H4n-uL <jun061119@gmail.com>
2025-05-06 01:15:24 +0900

    UPDATE :: Moving stack before heap init

--- kernel/src/ram.rs
+++ kernel/src/ram.rs
@@ -14,12 +14,12 @@ static ALLOCATOR: LockedHeap = LockedHeap::empty();
 pub fn init_ram(ember: &Ember) {
     let mut ramblock = RAM_BLOCK_MANAGER.lock();
     unsafe { arch::identity_map(ember, &mut ramblock); }
+    let stack_ptr = ramblock.alloc(STACK_SIZE, ramtype::CONVENTIONAL).unwrap();
+    ramblock.from_addr_mut(stack_ptr).unwrap().set_ty(ramtype::KERNEL_DATA);
+    unsafe { arch::move_stack(stack_ptr, STACK_SIZE); }
     let available = ramblock.available();
     let heap_size = ((available as f64 * 0.02) as usize).max(HEAP_SIZE);
     let heap_ptr = ramblock.alloc(heap_size, ramtype::CONVENTIONAL).unwrap();
     ramblock.from_addr_mut(heap_ptr).unwrap().set_ty(ramtype::KERNEL_DATA);
     unsafe { ALLOCATOR.lock().init(heap_ptr, heap_size); }
-    let stack_ptr = ramblock.alloc(STACK_SIZE, ramtype::CONVENTIONAL).unwrap();
-    ramblock.from_addr_mut(stack_ptr).unwrap().set_ty(ramtype::KERNEL_DATA);
-    unsafe { arch::move_stack(stack_ptr, STACK_SIZE); }
 }
