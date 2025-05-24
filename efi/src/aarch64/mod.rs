c47d7e1de926aeacce19a7885948a89639c58c3f
H4n-uL <jun061119@gmail.com>
2025-05-24 17:00:02 +0900

    UPDATE :: Added stack ptr getter

--- efi/src/aarch64/mod.rs
+++ efi/src/aarch64/mod.rs
@@ -1,4 +1,14 @@
+use aarch64_cpu::{asm::wfi, registers::DAIF};
+use tock_registers::interfaces::{Readable, Writeable};
+
 pub fn halt() {
-    aarch64::irq::nested_disable();
-    aarch64::instructions::halt();
+    DAIF.set(DAIF.get() | 0b1111);
+    wfi();
+}
+
+#[inline(always)]
+pub fn stack_ptr() -> usize {
+    let sp: usize;
+    unsafe { core::arch::asm!("mov {}, sp", out(reg) sp); }
+    return sp;
 }
