c76b8a1df7d6dea9b017e08c6727c9d5011e2233
H4n-uL <jun061119@gmail.com>
2025-05-28 20:10:40 +0900

    UPDATE :: Updated PCIe Device struct

--- kernel/src/device/mod.rs
+++ kernel/src/device/mod.rs
@@ -25,108 +25,119 @@ impl AcpiHandler for KernelAcpiHandler {
 
 #[derive(Clone, Copy, Debug)]
 pub struct PciDevice {
-    pub bus: u8,
-    pub device: u8,
-    pub function: u8,
-    pub header: PciHeader,
-    pub config: PciConfig
+    bus: u8,
+    device: u8,
+    function: u8,
+    ptr: *mut u32
 }
 
-#[repr(C)]
-#[derive(Clone, Copy, Debug)]
-pub struct PciHeader {
-    pub vendor_id: u16,
-    pub device_id: u16,
-    pub command: u16,
-    pub status: u16,
-    pub revision_id: u8,
-    pub prog_if: u8,
-    pub subclass: u8,
-    pub class: u8,
-    pub cache_line_size: u8,
-    pub latency_timer: u8,
-    pub header_type: u8,
-    pub bist: u8
-}
-
-#[derive(Clone, Copy, Debug)]
-pub enum PciConfig {
-    Type0(PciConfigType0),
-    Type1(PciConfigType1)
-}
-
-#[repr(C)]
-#[derive(Clone, Copy, Debug)]
-pub struct PciConfigType0 {
-    pub bar: [u32; 6],
-    pub cardbus_cis_ptr: u32,
-    pub subsystem_id: u16,
-    pub subsystem_vendor_id: u16,
-    pub expansion_rom_base: u32,
-    pub capabilities_ptr: u8,
-    pub reserved: [u8; 7],
-    pub interrupt_line: u8,
-    pub interrupt_pin: u8,
-    pub min_grant: u8,
-    pub max_latency: u8
-}
-
-#[repr(C)]
-#[derive(Clone, Copy, Debug)]
-pub struct PciConfigType1 {
-    pub bar: [u32; 2],
-    pub primary_bus: u8,
-    pub secondary_bus: u8,
-    pub subordinate_bus: u8,
-    pub secondary_latency: u8,
-    pub io_base: u8,
-    pub io_limit: u8,
-    pub secondary_status: u16,
-    pub memory_base: u16,
-    pub memory_limit: u16,
-    pub prefetch_memory_base: u16,
-    pub prefetch_memory_limit: u16,
-    pub prefetch_base_upper: u32,
-    pub prefetch_limit_upper: u32,
-    pub io_base_upper: u16,
-    pub io_limit_upper: u16,
-    pub capabilities_ptr: u8,
-    pub reserved0: [u8; 3],
-    pub expansion_rom_base: u32,
-    pub interrupt_line: u8,
-    pub interrupt_pin: u8,
-    pub bridge_control: u16
-}
+unsafe impl Send for PciDevice {}
+unsafe impl Sync for PciDevice {}
 
 impl PciDevice {
     pub fn read(mcfg_base: u64, bus: u8, device: u8, function: u8) -> Option<Self> {
-        let base_ptr = mcfg_base + ((bus as u64) << 20) + ((device as u64) << 15) + ((function as u64) << 12);
-        let header = unsafe { *(base_ptr as *const PciHeader) };
-        if header.vendor_id == 0xFFFF { return None; }
-
-        let config_ptr = base_ptr + size_of::<PciHeader>() as u64;
-        let config = unsafe {
-            match header.header_type & 0x7f {
-                0 => PciConfig::Type0(*(config_ptr as *const PciConfigType0)),
-                1 => PciConfig::Type1(*(config_ptr as *const PciConfigType1)),
-                _ => unreachable!()
-            }
-        };
-
-        return Some(PciDevice { bus, device, function, header, config });
+        let ptr = (mcfg_base + ((bus as u64) << 20) + ((device as u64) << 15) + ((function as u64) << 12)) as *mut u32;
+        let dev = PciDevice { bus, device, function, ptr };
+        if dev.vendor_id() == 0xFFFF { return None; }
+        return Some(dev);
     }
 
-    pub fn cfg(&self) -> &PciConfig { &self.config }
-    pub fn is_nvme(&self) -> bool { self.header.class == 0x01 && self.header.subclass == 0x08 }
-    pub fn is_vga(&self) -> bool { self.header.class == 0x03 && self.header.subclass == 0x00 }
-    pub fn is_bridge(&self) -> bool { self.header.header_type & 0x7f == 1 }
+    pub fn bus(&self) -> u8 { self.bus }
+    pub fn device(&self) -> u8 { self.device }
+    pub fn function(&self) -> u8 { self.function }
+    pub fn ptr(&self) -> *mut u32 { self.ptr }
+
+    pub fn enable_pci_device(&mut self) { self.set_command(self.command() | 0x0006); }
+
+    pub fn is_nvme(&self) -> bool { self.class() == 0x01 && self.subclass() == 0x08 }
+    pub fn is_vga(&self) -> bool { self.class() == 0x03 && self.subclass() == 0x00 }
+    pub fn is_bridge(&self) -> bool { self.is_type1() }
+
+    fn blob(&self) -> &[u32] { unsafe { core::slice::from_raw_parts(self.ptr, 16) } }
+    fn blob_mut(&self) -> &mut [u32] { unsafe { core::slice::from_raw_parts_mut(self.ptr, 16) } }
+
+    // Common methods
+    pub fn device_id(&self) -> u16       { (self.blob()[0] >> 16) as u16 }
+    pub fn vendor_id(&self) -> u16       {  self.blob()[0] as u16 }
+
+    pub fn status(&self) -> u16          { (self.blob()[1] >> 16) as u16 }
+    pub fn command(&self) -> u16         {  self.blob()[1] as u16 }
+    pub fn set_command(&mut self, command: u16) { self.blob_mut()[1] = ((self.status() as u32) << 16) | command as u32; }
+
+    pub fn class(&self) -> u8            { (self.blob()[2] >> 24) as u8 }
+    pub fn subclass(&self) -> u8         { (self.blob()[2] >> 16) as u8 }
+    pub fn prog_if(&self) -> u8          { (self.blob()[2] >> 8) as u8 }
+    pub fn reversion_id(&self) -> u8     {  self.blob()[2] as u8 }
+
+    pub fn bist(&self) -> u8             { (self.blob()[3] >> 24) as u8 }
+    pub fn header_type(&self) -> u8      { (self.blob()[3] >> 16) as u8 }
+    pub fn latency_timer(&self) -> u8    { (self.blob()[3] >> 8) as u8 }
+    pub fn cache_line_size(&self) -> u8  {  self.blob()[3] as u8 }
+
+    pub fn capabilities_ptr(&self) -> u8 {  self.blob()[13] as u8 }
+    pub fn interrupt_pin(&self) -> u8    { (self.blob()[15] >> 8) as u8 }
+    pub fn interrupt_line(&self) -> u8   {  self.blob()[15] as u8 }
+
+    pub fn bar(&self, index: usize) -> Option<u32> {
+        let val = self.blob()[4 + index];
+        match self.header_type() & 0x7f {
+            0 => { if index < 6 { Some(val) } else { None } },
+            1 => { if index < 2 { Some(val) } else { None } },
+            _ => None
+        }
+    }
+
+    pub fn expansion_rom_base(&self) -> u32 {
+        match self.header_type() & 0x7f {
+            0 => self.blob()[12],
+            1 => self.blob()[14],
+            _ => 0
+        }
+    }
+
+    // Type 0 specific methods
+    pub fn is_type0(&self) -> bool { self.header_type() & 0x7f == 0 }
+
+    pub fn cardbus_cis_ptr(&self) -> u32    {  self.blob()[10] }
+    pub fn subsys_id(&self) -> u16          { (self.blob()[11] >> 16) as u16 }
+    pub fn subsys_vendor_id(&self) -> u16   {  self.blob()[11] as u16 }
+
+    pub fn max_latency(&self) -> u8         { (self.blob()[15] >> 24) as u8 }
+    pub fn min_grant(&self) -> u8           { (self.blob()[15] >> 16) as u8 }
+
+    // Type 1 specific methods
+    pub fn is_type1(&self) -> bool { self.header_type() & 0x7f == 1 }
+
+    pub fn secondary_latency(&self) -> u8 { (self.blob()[6] >> 24) as u8 }
+    pub fn subordinate_bus(&self) -> u8 { (self.blob()[6] >> 16) as u8 }
+    pub fn secondary_bus(&self) -> u8 { (self.blob()[6] >> 8) as u8 }
+    pub fn primary_bus(&self) -> u8 { self.blob()[6] as u8 }
+
+    pub fn secondary_status(&self) -> u16 { (self.blob()[7] >> 16) as u16 }
+    pub fn io_limit(&self) -> u8 { (self.blob()[7] >> 8) as u8 }
+    pub fn io_base(&self) -> u8 { self.blob()[7] as u8 }
+
+    pub fn memory_limit(&self) -> u16 { (self.blob()[8] >> 16) as u16 }
+    pub fn memory_base(&self) -> u16 { self.blob()[8] as u16 }
+
+    pub fn prefetch_memory_limit(&self) -> u16 { (self.blob()[9] >> 16) as u16 }
+    pub fn prefetch_memory_base(&self) -> u16  { self.blob()[9] as u16 }
+
+    pub fn prefetch_base_upper(&self) -> u32   { self.blob()[10] }
+    pub fn prefetch_limit_upper(&self) -> u32  { self.blob()[11] }
+
+    pub fn io_limit_upper(&self) -> u16 { (self.blob()[12] >> 16) as u16 }
+    pub fn io_base_upper(&self) -> u16  {  self.blob()[12] as u16 }
+
+    pub fn bridge_control(&self) -> u16 { (self.blob()[15] >> 16) as u16 }
 }
 
 fn scan_pcie_devices(mcfg_base: u64, start_bus: u8, end_bus: u8) -> Vec<PciDevice> {
     let mut devices = Vec::new();
 
     for bus in start_bus..=end_bus { for device in 0..32 { for function in 0..8 {
-        if let Some(dev) = PciDevice::read(mcfg_base, bus, device, function) {
+        if let Some(mut dev) = PciDevice::read(mcfg_base, bus, device, function) {
+            dev.enable_pci_device();
             devices.push(dev);
         }
     }}}
@@ -183,9 +194,9 @@ pub fn init_device() {
     for dev in PCI_DEVICES.lock().iter() {
         printk!(
             "/bus{}/dev{}/fn{} | {:04x}:{:04x} Class {:02x}.{:02x} IF {:02x}",
-            dev.bus, dev.device, dev.function,
-            dev.header.vendor_id, dev.header.device_id,
-            dev.header.class, dev.header.subclass, dev.header.prog_if
+            dev.bus(), dev.device(), dev.function(),
+            dev.vendor_id(), dev.device_id(),
+            dev.class(), dev.subclass(), dev.prog_if()
         );
 
         if dev.is_nvme()   { printk!(" --> NVMe Controller"); }
